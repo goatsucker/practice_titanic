f70f10881d03dd77793720f763fd5f4f1033a900
W.J. Song <goatsucker@gmail.com>
2018-04-29 12:15:13 +0800

    preprocess training data, more reference columns are involved into the training

--- titanic.py
+++ titanic.py
@@ -2,6 +2,7 @@ import tensorflow
 import keras
 
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.models import Sequential
 from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
@@ -9,52 +10,43 @@ import pandas as pd
 
 
 def main():
-	train_df = pd.read_csv("train.csv")
+    train_df = pd.read_csv("train.csv")
 
-	# separate half of data for training, another portion for evaluate
+    # training data pre-process
+    train_predictors = train_df.drop(["Survived","PassengerId","Name","Ticket"], axis=1)
+    train_target = to_categorical(train_df[["Survived"]])
 
-	# training data pre-process
-	train_predictors = train_df[["Pclass", "Fare"]]
-	train_n_cols = train_predictors.shape[1]
-	train_target = to_categorical(train_df[["Survived"]])
+    train_predictors["Sex"] = train_predictors["Sex"].map({"female":0, "male":1}).astype(int)
+    train_predictors = pd.get_dummies(data = train_predictors, columns = ["Embarked"])
+    train_predictors = pd.get_dummies(data = train_predictors, columns = ["Cabin"])
+    train_predictors["Age"] = train_predictors["Age"].fillna(train_predictors["Age"].mean())
+    train_predictors["Fare"] = train_predictors["Fare"].fillna(train_predictors["Fare"].mean())
 
-	# testing data pre-process
-	#test_predictors = train_df.loc[train_df.shape[0]/2 + 1: , ["Pclass", "Fare"]]
-	#test_n_cols = test_predictors.shape[1]
-	#test_target = to_categorical(train_df.loc[train_df.shape[0]/2 + 1: , ["Survived"]])
+    # Set up the model
+    model = Sequential()
 
-	# Set up the model
-	model = Sequential()
+    # Add the first layer
+    model.add(Dense(100, activation="relu", input_shape = (train_predictors.shape[1],)))
 
-	# Add the first layer
-	model.add(Dense(50, activation="relu", input_shape = (train_n_cols,)))
+    model.add(Dense(300, activation="relu"))
 
-	model.add(Dense(32, activation="relu"))
+    model.add(Dense(32, activation="relu"))
+    
+    model.add(Dropout(0.2))
+
+    # output layer
+    model.add(Dense(2, activation="softmax"))
+
+    # Compile the model
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    #model.compile(optimizer="sdg", loss="mean_squared_error")
+
+    print("------- fit -------------")
+    early_stopping_monitor = EarlyStopping(patience = 10)
+
+    # Fit the model
+    model.fit(train_predictors, train_target, batch_size=100, epochs = 100, verbose=1, validation_split=0.3, callbacks=[early_stopping_monitor])
 	
-	# output layer
-	model.add(Dense(2))
-
-	# Compile the model
-	model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"])
-	#model.compile(optimizer="adam", loss="mean_squared_error")
-
-	print("------- summary -------------")
-	model.summary()
-
-	print("------- fit -------------")
-	early_stopping_monitor = EarlyStopping(patience = 3)
-
-	# Fit the model
-	model.fit(train_predictors, train_target, batch_size=100, epochs = 100, verbose=1, 
-		validation_split=0.3, callbacks=[early_stopping_monitor])
-
-	
-	print("------- evaluate -------------")
-	# evaluate
-	#score = model.evaluate(test_predictors, test_target, verbose=0)
-	#print(score)
-	#print('Test loss:', score[0])
-	#print('Test accuracy:', score[1])
 
 if __name__ == "__main__":
     # execute only if run as a script
